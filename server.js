e85e65ca2e24ee8edc9336c4dd4481fb6abe70b2
Kyle Anderson <kyleanderson@faceplacephoto.com>
2018-09-27 16:38:16 -0400

    Added pug

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+var path = require('path');
 var express = require('express');
 var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/view', function(req, res) {
+  res.render('index', {name:'kyle'})
+})
 
 //start a server on port 80 and log its start to our console
 var server = app.listen(80, function () {
